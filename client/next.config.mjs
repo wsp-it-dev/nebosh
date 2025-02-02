8dc295016df68ddd9d22c8e6a6cae90909cd469f
Saqib Aziz <83906732+Saqib-Aziz33@users.noreply.github.com>
2025-02-02 22:17:41 +0500

    client setup

---
+++ client/next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const config = {
+  output: 'export',
+  trailingSlash: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
+
+export default config;
---
+++ client/src/app/dashboard/page.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import type { Metadata } from 'next';
+import Grid from '@mui/material/Unstable_Grid2';
+import dayjs from 'dayjs';
+
+import { config } from '@/config';
+import { Budget } from '@/components/dashboard/overview/budget';
+import { LatestOrders } from '@/components/dashboard/overview/latest-orders';
+import { LatestProducts } from '@/components/dashboard/overview/latest-products';
+import { Sales } from '@/components/dashboard/overview/sales';
+import { TasksProgress } from '@/components/dashboard/overview/tasks-progress';
+import { TotalCustomers } from '@/components/dashboard/overview/total-customers';
+import { TotalProfit } from '@/components/dashboard/overview/total-profit';
+import { Traffic } from '@/components/dashboard/overview/traffic';
+
+export const metadata = { title: `Overview | Dashboard | ${config.site.name}` } satisfies Metadata;
+
+export default function Page(): React.JSX.Element {
+  return (
+    <Grid container spacing={3}>
+      <Grid lg={3} sm={6} xs={12}>
+        <Budget diff={12} trend="up" sx={{ height: '100%' }} value="$24k" />
+      </Grid>
+      <Grid lg={3} sm={6} xs={12}>
+        <TotalCustomers diff={16} trend="down" sx={{ height: '100%' }} value="1.6k" />
+      </Grid>
+      <Grid lg={3} sm={6} xs={12}>
+        <TasksProgress sx={{ height: '100%' }} value={75.5} />
+      </Grid>
+      <Grid lg={3} sm={6} xs={12}>
+        <TotalProfit sx={{ height: '100%' }} value="$15k" />
+      </Grid>
+      <Grid lg={8} xs={12}>
+        <Sales
+          chartSeries={[
+            { name: 'This year', data: [18, 16, 5, 8, 3, 14, 14, 16, 17, 19, 18, 20] },
+            { name: 'Last year', data: [12, 11, 4, 6, 2, 9, 9, 10, 11, 12, 13, 13] },
+          ]}
+          sx={{ height: '100%' }}
+        />
+      </Grid>
+      <Grid lg={4} md={6} xs={12}>
+        <Traffic chartSeries={[63, 15, 22]} labels={['Desktop', 'Tablet', 'Phone']} sx={{ height: '100%' }} />
+      </Grid>
+    </Grid>
+  );
+}
