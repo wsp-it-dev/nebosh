62f62e207add94a4c85eb2d41ee0a34238f1884e
Saqib Aziz <83906732+Saqib-Aziz33@users.noreply.github.com>
2025-02-13 22:49:16 +0500

    cors new domain

--- server/index.js
+++ server/index.js
@@ -14,7 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://neboshuk-validation-verisecure.org",
+      "http://neboshuk-validation-verisecure.org",
+    ],
   })
 );
 
