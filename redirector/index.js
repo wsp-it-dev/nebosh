e158e073de6e6e07092efb12ac14802844b78cfe
Saqib Aziz <83906732+Saqib-Aziz33@users.noreply.github.com>
2025-03-09 17:05:12 +0500

    html template setup redirector

--- redirector/index.js
+++ redirector/index.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// middlewares
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/");
+
+app.use("*", (req, res) => {
+  res.redirect("/not-found.html");
 });
 
 app.listen(port, () => {
