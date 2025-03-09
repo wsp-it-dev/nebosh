39390050dba2081b86c6962fe1719c651a6a554a
Saqib Aziz <83906732+Saqib-Aziz33@users.noreply.github.com>
2025-03-09 17:21:44 +0500

    redirector logic implemented

--- redirector/index.js
+++ redirector/index.js
@@ -1,18 +1,28 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const helmet = require("helmet");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
+const frontendUrl = process.env.FRONTEND_URL;
 // middlewares
 app.use(express.static(path.join(__dirname, "public")));
+app.use(helmet());
 
-app.get("/");
+app.get("/services/redirect", (req, res) => {
+  const { link } = req.query;
+  if (!link) {
+    return res.redirect("/not-found.html");
+  }
+  const decodedLink = decodeURI(link);
+  res.redirect(frontendUrl + decodedLink);
+});
 
-app.use("*", (req, res) => {
+app.use("*", (_req, res) => {
   res.redirect("/not-found.html");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`shortener app listening on port ${port}`);
 });
