1113c500a32bfa5ce81fb6c375c300dc21780da6
Saqib Aziz <83906732+Saqib-Aziz33@users.noreply.github.com>
2025-03-13 15:16:53 +0500

    delete confirm

--- client/src/components/dashboard/certificates/delete-cert.tsx
+++ client/src/components/dashboard/certificates/delete-cert.tsx
@@ -1,27 +1,59 @@
+import React, { useState } from 'react';
 import apiService from '@/services/api.service';
-import { IconButton } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton } from '@mui/material';
 import { Trash } from '@phosphor-icons/react';
 import { useMutation, useQueryClient } from 'react-query';
 import { toast } from 'react-toastify';
 
 function DeleteCert({ id }: { id: number }) {
   const queryClient = useQueryClient();
+  const [open, setOpen] = useState(false);
 
   const { isLoading, mutate } = useMutation(async () => {
-    await apiService.delete(`/api/certificates/${id}`);
-    queryClient.invalidateQueries(`certificates`);
-    toast.success('certificate deleted');
     try {
+      await apiService.delete(`/api/certificates/${id}`);
+      queryClient.invalidateQueries('certificates');
+      toast.success('Certificate deleted');
     } catch (e: any) {
-      const msg = e?.response?.data?.message || 'something went wrong';
+      const msg = e?.response?.data?.message || 'Something went wrong';
       toast.error(msg);
     }
   });
 
+  const handleDelete = () => {
+    mutate();
+    setOpen(false);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <IconButton onClick={() => mutate()} disabled={isLoading}>
-      <Trash size={16} />
-    </IconButton>
+    <>
+      <IconButton onClick={handleClickOpen} disabled={isLoading}>
+        <Trash size={16} />
+      </IconButton>
+
+      {/* Confirmation Dialog */}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Confirm Deletion</DialogTitle>
+        <DialogContent>Are you sure you want to delete this certificate({id})?</DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleDelete} color="secondary" disabled={isLoading}>
+            {isLoading ? 'Deleting...' : 'Delete'}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
+
 export default DeleteCert;
